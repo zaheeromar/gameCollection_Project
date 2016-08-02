7e785c6a501180f4ae7c38269cf17885ab618bdc
eewfewfewf <dehdfkjwfew>
2016-08-02 16:34:27 +0100

    updates

--- mvcGameCollection/mvcGameCollection/App_Start/RouteConfig.cs
+++ mvcGameCollection/mvcGameCollection/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace mvcGameCollection
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Games", action = "Index", id = UrlParameter.Optional }
             );
 
            routes.MapRoute(
